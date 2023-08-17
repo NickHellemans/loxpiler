6a07efce7d8451f5eda0aaceed4cbeb1d338ce98
NickHellemans <60689153+NickHellemans@users.noreply.github.com>
2023-08-17 19:06:28 +0200

    Add line information to bytecode

--- loxpiler/src/chunk.c
+++ loxpiler/src/chunk.c
@@ -5,18 +5,21 @@
 void init_chunk(Chunk* chunk) {
 	chunk->size = 0;
 	chunk->capacity = 0;
+	chunk->lines = NULL;
 	chunk->code = NULL;
 	init_value_array(&chunk->constants);
 }
 
-void write_chunk(Chunk* chunk, uint8_t byte) {
+void write_chunk(Chunk* chunk, uint8_t byte, int line) {
 	if(chunk->size >= chunk->capacity) {
 		const int oldCap = chunk->capacity;
 		chunk->capacity = GROW_CAPACITY(oldCap);
 		chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCap, chunk->capacity);
+		chunk->lines = GROW_ARRAY(int, chunk->lines, oldCap, chunk->capacity);
 	}
 
 	chunk->code[chunk->size] = byte;
+	chunk->lines[chunk->size] = line;
 	chunk->size++;
 }
 
@@ -28,6 +31,7 @@ int add_constant(Chunk* chunk, Value value) {
 
 void free_chunk(Chunk* chunk) {
 	//Deallocate memory
+	FREE_ARRAY(int, chunk->lines, chunk->capacity);
 	FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
 	free_value_array(&chunk->constants);
 	//Reset to empty state
--- loxpiler/src/chunk.h
+++ loxpiler/src/chunk.h
@@ -13,11 +13,13 @@ typedef struct {
 	int size;
 	int capacity;
 	uint8_t* code;
+	//Store line numbers parallel with bytecode for errors
+	int* lines;
 	//Constant pool to store every constant
 	ValueArray constants;
 } Chunk;
 
 void init_chunk(Chunk* chunk);
-void write_chunk(Chunk* chunk, uint8_t byte);
+void write_chunk(Chunk* chunk, uint8_t byte, int line);
 int add_constant(Chunk* chunk, Value value);
 void free_chunk(Chunk* chunk);
