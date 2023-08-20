bf88729a95bde5562a4b16aabbb69f5edc95da0b
NickHellemans <60689153+NickHellemans@users.noreply.github.com>
2023-08-20 23:23:45 +0200

    Add hash table: delete

--- loxpiler/src/table.c
+++ loxpiler/src/table.c
@@ -22,12 +22,28 @@ void free_table(Table* table) {
 
 Entry* find_entry(Entry* entries, int cap, ObjString* key) {
 	uint32_t index = key->hash % cap;
+	//Check for tombstones while linear probing to not miss any possible entries
 
+	Entry* tombstone = NULL;
 	for(;;) {
 		Entry* entry = &entries[index];
-		if(entry->key == key || entry->key == NULL) {
-			return entry;
+		if(entry->key == NULL) {
+			if (IS_NIL(entry->value))
+				//Empty entry
+				//If tombstone is not NULL it means we passed a tombstone
+				//so we return that location instead of current entry to reuse it to avoid wasting space
+				//else return entry
+				return tombstone != NULL ? tombstone : entry;
+			else
+				//Found tombstone, update var and keep going with next location
+				if (tombstone == NULL)
+					tombstone = entry;
 		}
+		//Found key
+		//We can compare pointers because string interning
+		else if (entry->key == key)
+			return entry;
+		
 		//Linear probing to handle collisions
 		index = (index + 1) % cap;
 	}
@@ -41,6 +57,9 @@ static void adjust_capacity(Table* table, int cap) {
 		entries[i].value = NIL_VAL;
 	}
 
+	//When rehashing we don't copy tombstones so need to recalc size
+	//Rebuild probe sequences anyways
+	table->size = 0;
 	//Rehash elems
 	for(int i = 0; i < table->cap; i++) {
 		Entry* entry = &table->elements[i];
@@ -49,6 +68,7 @@ static void adjust_capacity(Table* table, int cap) {
 		Entry* dest = find_entry(entries, cap, entry->key);
 		dest->key = entry->key;
 		dest->value = entry->value;
+		table->size++;
 	}
 
 	//Free old table
@@ -79,12 +99,30 @@ bool table_set(Table* table, ObjString* key, Value value) {
 	Entry* entry = find_entry(table->elements, table->cap, key);
 	bool isNewKey = entry->key == NULL;
 
-	if (isNewKey)
+	//Only increment count when we insert in a totally empty entry and not a tombstone or new key
+	//Bucket with tombstone has been accounted for
+	if (isNewKey && IS_NIL(entry->value))
 		table->size++;
 
 	entry->key = key;
 	entry->value = value;
 	return isNewKey;
+}
+
+//Does not decrease size --> deleted entries turn into tombstones
+//tombstones get counted in size 
+bool table_delete(Table* table, ObjString* key) {
+	if (table->size == 0)
+		return false;
+	//Look for entry
+	Entry* entry = find_entry(table->elements,table->cap, key);
+	//Not in table
+	if (entry->key == NULL)
+		return false;
+	//Delete key from table and set tombstone (val = true)
+	entry->key = NULL;
+	entry->value = BOOL_VAL(true);
+	return true;
 }
 
 void table_add_all(Table* from, Table* to) {
